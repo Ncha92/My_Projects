6baa1d46e97528300e4fc097efeda1ef0ba5ec13
ntshia cha <txhiaj92@gmail.com>
2020-03-12 11:34:24 -0400

    Spring Boot project

---
+++ springproject/src/test/java/com/boot_project/SpringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
